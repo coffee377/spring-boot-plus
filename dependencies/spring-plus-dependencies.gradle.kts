5ec6214a9afcbb9a74e1727fb9acad50d4a3cf26
Wu Yujie <coffee377@dingtalk.com>
2022-11-17 16:08:44 +0800

    :zap: 统一版本依赖

--- dependencies/spring-plus-dependencies.gradle.kts
+++ dependencies/spring-plus-dependencies.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
   constraints {
     /* 授权服务器依赖 */
     api(libs.spring.authorization.server)
+    api(libs.bundles.dingtalk)
     api("com.baomidou:mybatis-plus-boot-starter:3.5.2")
 
     api("org.mapstruct:mapstruct:1.5.0.RC1")
@@ -41,6 +42,16 @@ publishing {
     maven {
       name = "local"
       url = uri("${project.rootProject.buildDir}/publications/repos")
+    }
+    maven {
+      name = "nexus3"
+      val versionType = if (project.version.toString().endsWith("", true)) "snapshots" else "releases"
+      url = uri("http://nexus.jqk8s.jqsoft.net/repository/maven-${versionType}/")
+      isAllowInsecureProtocol = true
+      credentials {
+        username = System.getenv("DEV_OPTS_JQ_USERNAME")
+        password = System.getenv("DEV_OPTS_JQ_PASSWORD")
+      }
     }
   }
   publications {
